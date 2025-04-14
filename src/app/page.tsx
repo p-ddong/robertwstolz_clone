86b7ccf6e6c71c52be43c14ed764c9fd98ce38c4
Hankrezu <hankrezu@gmail.com>
2025-04-14 16:03:06 +0700

    fix 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,37 @@
 import Header from "./Components/Header/Header";
 import Carousel from "./Components/Carousel/Carousel";
 import Content from "./Components/Content/Content";
-import './page.scss'
+import "./page.scss";
 import ContactForm from "./Components/ContactForm/ContactForm";
 import Footer from "./Components/Footer/Footer";
 
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
   return (
     <div>
       <Header />
       <Carousel />
       <Content />
-      <div className="vimeo_video"/>
-      <ContactForm/>
-      <Footer/>
+      <div className="vimeo_video">
+        <iframe
+          title="vimeo-player"
+          src="https://player.vimeo.com/video/743911758?h=17a66078f1"
+          height= "600px"
+          width= "337.5px"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <ContactForm />
+      <Footer />
     </div>
   );
 }
