b18f81197a21624ecaea1926f89ce31f95c6785c
Hankrezu <hankrezu@gmail.com>
2025-04-21 17:15:35 +0700

    fix 5

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -9,6 +9,7 @@ import { CiUser, CiSearch } from "react-icons/ci";
 import { PiHandbag } from "react-icons/pi";
 import { CiMenuBurger } from "react-icons/ci";
 import { FaChevronDown } from "react-icons/fa6";
+import { MdDelete } from "react-icons/md";
 
 import Image from "next/image";
 import { ClothingItem } from "../Constant/types";
@@ -25,28 +26,46 @@ const Header = ({ cart, setCart }: Props) => {
   const manageItems = (type: string, id: number) => {
     const index = cart.findIndex((item) => item.id === id);
     if (index === -1) return console.warn("Item not found:", id);
-  
+
     const item = cart[index];
     const quantity = item.quantity ?? 0;
-  
+
     const updatedItem = {
       ...item,
-      quantity:
-        type === "add" ? quantity + 1 : quantity > 1 ? quantity - 1 : 0,
+      quantity: type === "add" ? quantity + 1 : quantity > 1 ? quantity - 1 : 0,
       isInCart: type === "add" || quantity > 1,
     };
-  
+
     const newCart = [...cart];
     newCart[index] = updatedItem;
     setCart(newCart);
   };
 
   const totalCal = () => {
-    const total = cart.filter(item => item.isInCart).reduce((acc, item) => {
-      const price = item.sale ? item.price : item.price - 30;
-      return acc + price * (item.quantity ?? 1);
-    }, 0);
+    const total = cart
+      .filter((item) => item.isInCart)
+      .reduce((acc, item) => {
+        const price = item.sale ? item.price : item.price - 30;
+        return acc + price * (item.quantity ?? 1);
+      }, 0);
     return total;
+  };
+
+  const deleteFromcart = (id: number) => {
+    const index = cart.findIndex((item) => item.id === id);
+    if (index === -1) return console.warn("Item not found:", id);
+
+    const item = cart[index];
+
+    const updatedItem = {
+      ...item,
+      quantity: 0,
+      isInCart: false,
+    };
+
+    const newCart = [...cart];
+    newCart[index] = updatedItem;
+    setCart(newCart);
   };
 
   useEffect(() => {
@@ -95,11 +114,31 @@ const Header = ({ cart, setCart }: Props) => {
 
               {cartItems.length >= 1 ? (
                 <div className="cart_items">
-                  <div className="list space-y-3">
+                  <h1>Your Cart</h1>
+                  <table className="list">
+                    <tr>
+                      <th className="name">Product</th>
+                      <th className="price">Price</th>
+                      <th className="quantity">Quantity</th>
+                      <th className="delete">Delete</th>
+                    </tr>
                     {cartItems.map((item) => (
-                      <div className="cart_item" key={item.id}>
-                        <p>{item.name}</p>
-                        <div>
+                      <tr key={item.id} className="cart_item">
+                        <td>{item.name}</td>
+                        <td>
+                          {item.sale ? (
+                            `${item.price}$`
+                          ) : (
+                            <>
+                              <span className="line-through mr-2">
+                                {item.price}$
+                              </span>
+                              &nbsp;
+                              <span>{item.price - 30}$</span>
+                            </>
+                          )}
+                        </td>
+                        <td className="quantity_manage">
                           <button
                             onClick={() => manageItems("subtract", item.id)}
                           >
@@ -109,11 +148,19 @@ const Header = ({ cart, setCart }: Props) => {
                           <button onClick={() => manageItems("add", item.id)}>
                             +
                           </button>
-                        </div>
-                      </div>
+                        </td>
+                        <td className="clear">
+                          <button onClick={() => deleteFromcart(item.id)}>
+                            <MdDelete className="text-red-400" />
+                          </button>
+                        </td>
+                      </tr>
                     ))}
+                  </table>
+
+                  <div className="total">
+                    <p>Total</p> <p>{totalCal()} $</p>
                   </div>
-                  <div className="total"><p>Total</p> <p>{totalCal()}</p></div>
                 </div>
               ) : (
                 ""
@@ -230,30 +277,35 @@ const Header = ({ cart, setCart }: Props) => {
             <PiHandbag />
             <p className="txt">{cartItems.length}</p>
             {cartItems.length >= 1 ? (
-                <div className="cart_items">
-                  <div className="list space-y-3">
-                    {cartItems.map((item) => (
-                      <div className="cart_item" key={item.id}>
-                        <p>{item.name}</p>
-                        <div>
-                          <button
-                            onClick={() => manageItems("subtract", item.id)}
-                          >
-                            -
-                          </button>
-                          <p>{item.quantity}</p>
-                          <button onClick={() => manageItems("add", item.id)}>
-                            +
-                          </button>
-                        </div>
+              <div className="cart_items">
+                <div className="list space-y-3">
+                  {cartItems.map((item) => (
+                    <div className="cart_item" key={item.id}>
+                      <p>{item.name}</p>
+                      <div>
+                        <button
+                          onClick={() => manageItems("subtract", item.id)}
+                        >
+                          -
+                        </button>
+                        <p>{item.quantity}</p>
+                        <button onClick={() => manageItems("add", item.id)}>
+                          +
+                        </button>
                       </div>
-                    ))}
-                  </div>
-                  <div className="total"><p>Total</p> <p>{totalCal()}</p></div>
+                      <button onClick={() => deleteFromcart(item.id)}>
+                        <MdDelete className="text-red-400 text-xl" />
+                      </button>
+                    </div>
+                  ))}
                 </div>
-              ) : (
-                ""
-              )}
+                <div className="total">
+                  <p>Total</p> <p>{totalCal()}$</p>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </div>
